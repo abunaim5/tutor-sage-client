3289a5495c82f348111261ee759e5bc00ccf3da5
abunaim5 <abunaim140@gmail.com>
2024-06-01 18:59:24 +0600

    added bannner section with styles

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import { Box } from "@chakra-ui/react";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home</h1>
-        </div>
+        <Box>
+            <Banner />
+        </Box>
     );
 };
 
--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <Box p={8} w='full'>
+        <Box p={7} w='full'>
             <Flex maxW='8xl' mx='auto' alignItems='center' gap='2' px={2}>
                 <Box>
                     <Heading size='lg' as={Link} to='/' textColor='#252525' fontFamily='logo.croissant' >TutorSa<span className="text-[#FF1949]">g</span>e</Heading>
