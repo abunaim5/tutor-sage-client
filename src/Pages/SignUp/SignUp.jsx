7799bf5a33938aaa89a830e4e19f9f8376fdee18
abunaim5 <abunaim140@gmail.com>
2024-06-01 23:50:59 +0600

    added sign up page component

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -3,29 +3,42 @@ import { Box, Button, ButtonGroup, Card, FormControl, FormLabel, Heading, IconBu
 import { FaFacebookF, FaGoogle, FaTwitter } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const LogIn = () => {
+const SignUp = () => {
     return (
         <Box h='calc(100vh - 104px)' display='flex' justifyContent='center' alignItems='center' >
             <Card minW='lg' p={6} rounded='none'>
-                <Heading textAlign='center' textColor='#252525'>Sign In</Heading>
+                <Heading textAlign='center' textColor='#252525'>Sign Up</Heading>
                 <form className="mt-8">
                     <FormControl isRequired>
+                        <FormLabel>Name</FormLabel>
+                        <Input type="text" placeholder='Name' borderRadius='none' focusBorderColor="primary.300" />
+                    </FormControl>
+                    <FormControl isRequired my={6}>
                         <FormLabel>Email</FormLabel>
                         <Input type="email" placeholder='Email' borderRadius='none' focusBorderColor="primary.300" />
                     </FormControl>
-                    <FormControl isRequired mt={6}>
+                    <FormControl isRequired>
                         <FormLabel>Password</FormLabel>
                         <InputGroup>
                             <Input type="password" placeholder='Password' borderRadius='none' focusBorderColor="primary.300" />
                             <InputRightElement>
-                                <ViewIcon fontSize='18px' color='gray' />
+                                <ViewIcon color='gray' />
                             </InputRightElement>
                         </InputGroup>
                     </FormControl>
-                    <Button type="submit" colorScheme="primary" borderRadius='none' mt={6} w='100%'>Log In</Button>
+                    <FormControl isRequired mt={6}>
+                        <FormLabel>Your Photo</FormLabel>
+                        <Input type='text' placeholder='https://' borderRadius='none' focusBorderColor="primary.300" />
+                    </FormControl>
+                    {/* TODO: Implement choose file */}
+                    {/* <FormControl isRequired mt={6}>
+                        <FormLabel>Your Photo</FormLabel>
+                        <Input type='file' borderRadius='none' border='none' px={0} />
+                    </FormControl> */}
+                    <Button type="submit" colorScheme="primary" borderRadius='none' mt={6} w='100%'>Register</Button>
                 </form>
                 <Box mt={6}>
-                    <Text textAlign='center'>Login with social accounts</Text>
+                    <Text textAlign='center'>SignUp with social accounts</Text>
                     <ButtonGroup justifyContent='center' w='100%' mt={3}>
                         <IconButton
                             isRound
@@ -54,9 +67,9 @@ const LogIn = () => {
                     </ButtonGroup>
                 </Box>
                 <Text textAlign='center' mt={6}>
-                    New Here?{' '}
-                    <Link to='/register'>
-                        Create an account!
+                    Already have an account?{' '}
+                    <Link to='/login'>
+                        Sign In
                     </Link>
                 </Text>
             </Card>
@@ -64,4 +77,4 @@ const LogIn = () => {
     );
 };
 
-export default LogIn;
+export default SignUp;
--- src/Routes/Routes/Routes.jsx
+++ src/Routes/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../../Pages/Home/Home/Home";
 import Main from "../../Layout/Main";
 import LogIn from "../../Pages/LogIn/LogIn";
+import SignUp from "../../Pages/SignUp/SignUp";
 
 
 const router = createBrowserRouter([
@@ -15,9 +16,13 @@ const router = createBrowserRouter([
           element: <Home />
         },
         {
+          path: '/register',
+          element: <SignUp />
+        },
+        {
           path: '/login',
           element: <LogIn />
-        }
+        },
       ],
     },
   ]);
